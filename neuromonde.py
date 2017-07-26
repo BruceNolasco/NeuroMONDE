007cbe125d8279e842eb3f97e21bf8425fa3943b
BruceNolasco <daecherd@gmail.com>
2017-07-26 16:10:16 -0500

    Neuromonde's interface

--- neuromonde.py
+++ neuromonde.py
@@ -2,32 +2,37 @@ import numpy as np
 import MtoNCoupling as cp
 #import mnist_loader as mn
 import network
-import matplotlib.pyplot as plt
 
-gens = 20
+print "Welcome to NEUROMONDE SGD Training Tool"
+print "Enter the number of gens: "
+
+gens =int(raw_input())
+topology = [16,32,32,16]
+print "Defaults are T="+str(topology)
+askplot = raw_input("Plot? Y/n  ")
+
 traindata = cp.getTrainData("relSignalsC.txt","pointListC.txt")[::2]
 net = network.Network([16,32,32,16])
 test_data = [(x[0],cp.binToCoord(x[1],8))for x in traindata [::60]]
-#print [(x[0],x[1]) for x in test_data]
-#print traindata[:5]
-#print mn.load_data_wrapper()[0][:1]
+
 net.SGD(traindata,gens,100,0.1,test_data=test_data)
 
-means =[x[0] for x in net.history]
-stds = [float(x[1]) for x in net.history]
+if askplot == "Y":
 
-base = [j for j in range(gens)]
-mini = [ x[2] for x in net.history]
-maxi = [ x[3] for x in net.history]
-minimax = []
-minimax.append([mini])
-minimax.append([maxi])
-print minimax
+	import matplotlib.pyplot as plt
+	means =[x[0] for x in net.history]
+	stds = [float(x[1]) for x in net.history]
 
-fig, ax = plt.subplots()
-ax.errorbar(base, means, yerr =stds)
-ax.plot(base,mini,color="red")
-ax.plot(base,maxi,color="green")
-plt.show()
+	base = [j for j in range(gens)]
+	mini = [ x[2] for x in net.history]
+	maxi = [ x[3] for x in net.history]
+	minimax = []
+	minimax.append([mini])
+	minimax.append([maxi])
 
-plt.show()
+	fig, ax = plt.subplots()
+	ax.errorbar(base, means, yerr =stds)
+	ax.plot(base,mini,color="red")
+	ax.plot(base,maxi,color="green")
+	plt.show()
+
